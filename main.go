0e4dac643febc02c3c23e29bc354ab969cdd4808
ultra0202 <gn76@mail.ru>
2023-01-21 23:48:34 +0400

    reading input string with os.Args

--- main.go
+++ main.go
@@ -1,18 +1,13 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	var str string
-	flag.String(str, "", "****")
-	flag.Parse()
-	src := flag.Args()
-	//fmt.Printf("%T %v %d\n", src, src, len(src))
-	//fmt.Println("**", src[0], "**")
-	e := strings.Fields(src[0])
+	str := os.Args[1]
+	e := strings.Fields(str)
 	fmt.Println(len(e))
 }
